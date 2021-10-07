b616471ada14e7dc8a03dfda96ba0ed8b6c18432
dragonman164 <sanidhiyafirefox123@gmail.com>
2021-10-08 00:24:50 +0530

    removed extra folder

---
+++ Download Desired Images (Web)/DownloadImages.py
@@ -0,0 +1,32 @@
+from google_images_download import google_images_download
+response = google_images_download.googleimagesdownload()
+search_queries =['apple',
+]
+
+
+def downloadimages(query):
+	arguments = {"keywords": query,
+				"format": "jpg",
+				"limit":5,
+				"print_urls":True,
+				"size": "medium",
+				"aspect_ratio":"panoramic"}
+	try:
+		response.download(arguments)
+	
+	except FileNotFoundError:
+		arguments = {"keywords": query,
+					"format": "jpg",
+					"limit":4,
+					"print_urls":True,
+					"size": "medium"}
+					
+	
+		try:
+			response.download(arguments)
+		except:
+			pass
+
+for query in search_queries:
+	downloadimages(query)
+	print()
